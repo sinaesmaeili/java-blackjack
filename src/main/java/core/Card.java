3ab448f8fcef43656e3fbb4425fcc15ef1ece1b8
sinaesmaeili <sina.esmaeili01@gmail.com>
2018-09-09 18:02:53 -0400

    Implemented Card logic

--- src/main/java/core/Card.java
+++ src/main/java/core/Card.java
@@ -2,5 +2,34 @@ package core;
 
 public class Card {
 
+    private String suit;
+    private String rank;
+
+    public Card(String suit, String rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    public String printCard() {
+        return this.suit + this.rank;
+    }
+
+    public int getCardValue() {
+        if (this.rank.equals("K") || this.rank.equals("Q") || this.rank.equals("J")) {
+            return 10;
+        } else if (this.rank.equals("A")) {
+            return 11;
+        } else {
+            return Integer.parseInt(this.rank);
+        }
+    }
+
+    public String getSuit() {
+        return this.suit;
+    }
+
+    public String getRank() {
+        return this.rank;
+    }
 
 }
